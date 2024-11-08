31f7209b9e42e060921390fc513d28077e0ed96c
Bolezni <Yurchenkods05@gmail.com>
2024-11-08 19:38:08 +0300

    новый метод в NotificationService и еще что-то

--- src/main/java/org/example/testprojectback/repository/FriendshipRepository.java
+++ src/main/java/org/example/testprojectback/repository/FriendshipRepository.java
@@ -4,7 +4,6 @@ import org.example.testprojectback.model.Friendship;
 import org.example.testprojectback.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
